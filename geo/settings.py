bd647464e33d5bce54b1e297e1e8f3db215113cd
Max Petrov <mak_sim93@mail.ru>
2019-04-11 14:01:24 +0300

    add static files setting

--- geo/settings.py
+++ geo/settings.py
@@ -126,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myStatic'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
+MEDIA_URL = '/media/'
